c31de81799d227b772b69b951f8d0b4b85b95d67
Cicadezz <1262260557@qq.com>
2023-07-19 19:12:57 +0800

    第一次上传

---
+++ src/components/datav/BottomCharts.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <div class="bottom-charts">
+    <div class="bc-header">
+      <div>最新新闻咨询</div>
+      <div @click="goPage1('http://datav.jiaminghi.com/')">点击此处跳转至新闻页面1</div>
+      <div @click="goPage2('https://github.com/')">点击此处跳转至新闻页面2</div>
+      <div @click="goPage3('http:///www.baidu.com')">点击此处跳转至新闻页面3</div>
+      <div @click="goPage4('http://www.xinhuanet.com/')">点击此处跳转至新闻页面4</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goPage1 (url) {
+      window.open(url, '_blank')
+    },
+    goPage2 (url) {
+      window.open(url, '_blank')
+    },
+    goPage3 (url) {
+      window.open(url, '_blank')
+    },
+    goPage4 (url) {
+      window.open(url, '_blank')
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.bottom-charts {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: relative;
+
+  .bc-header{
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    font-size:20px;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/datav/LeftChart1.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="left-chart-1">
+    <div class="lc1-header">T5中心信息</div>
+    <div class="lc1-details">本月事件总数<span>220</span></div>
+    <dv-capsule-chart class="lc1-chart" :config="config" />
+    <dv-decoration-2 style="height:10px;" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftChart1',
+  data () {
+    return {
+      config: {
+        data: [
+          {
+            name: '拜访客户数量',
+            value: 160
+          },
+          {
+            name: '招投标数',
+            value: 60
+          }
+        ],
+        colors: ['#00baff', '#3de7c9'],
+        unit: '数'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.left-chart-1 {
+  width: 100%;
+  height: 37%;
+  display: flex;
+  flex-grow: 0;
+  flex-direction: column;
+
+  .lc1-header {
+    text-align: center;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
+    margin-bottom: 20px;
+  }
+
+  .lc1-details {
+    height: 50px;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    text-indent: 20px;
+
+    span {
+      color: #096dd9;
+      font-weight: bold;
+      font-size: 35px;
+      margin-left: 20px;
+    }
+  }
+
+  .lc1-chart {
+    flex: 1;
+  }
+}
+</style>
